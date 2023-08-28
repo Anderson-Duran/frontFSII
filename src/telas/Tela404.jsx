064e0d0845d6947bef07d5479f8453113ce20872
Anderson-Duran <andersonduran@hotmail.com>
2023-08-28 12:45:08 -0300

    fix:bugs

--- src/telas/Tela404.jsx
+++ src/telas/Tela404.jsx
@@ -4,7 +4,7 @@ import Pagina from "../templates/Pagina";
 export default function Tela404(props){
     return (
         <Pagina>
-            <Alert className="text-center" variant="warning">O recurso solicitado não existe</Alert>
+            
         </Pagina>
     );
 }
