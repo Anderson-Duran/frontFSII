2372ce218fb6bb37fdc90c3862f8803cebe1bb63
Anderson-Duran <andersonduran@hotmail.com>
2023-09-09 15:10:20 -0300

    fix

--- src/templates/selectionBox/SelectionBox.jsx
+++ src/templates/selectionBox/SelectionBox.jsx
@@ -21,6 +21,7 @@ function SelectionBox({ source, dataKey, exhibitionField, selectFunction }) {
     const fetchData = async () => {
       let response = await fetch(source, { method: "GET" })
       let result = await response.json();
+      console.log('resultadoFetch',result)
       let newList = filterAndCapitalizeName(result)
       setData(newList)
       console.log(newList, data)
